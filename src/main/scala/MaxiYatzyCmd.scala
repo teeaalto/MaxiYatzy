bd853262e41ece28e202673d195baf51e4c02071
Tuomas Aalto <tuomas.e.e.aalto@gmail.com>
2018-03-27 15:19:45 +0300

    Initial commit with a test source file

---
+++ src/main/scala/MaxiYatzyCmd.scala
@@ -0,0 +1,9 @@
+object MaxiYatzyCmd {
+  def main(args: Array[String]) = {
+    var input = ""
+    while (input != "lopeta") {
+      println("Sa jotai")
+      input = scala.io.StdIn.readLine()
+    }
+  }
+}
