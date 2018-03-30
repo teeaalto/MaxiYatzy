d0812f40115e7fc6b060fffc35f0a2fb851bc8d4
Tuomas Aalto <tuomas.e.e.aalto@gmail.com>
2018-03-30 22:17:32 +0300

    Simplified the implementation of cmd controller

--- src/main/scala/maxiYatzy/Players.scala
+++ src/main/scala/maxiYatzy/Players.scala
@@ -5,9 +5,5 @@ import scala.collection.mutable.ArrayBuffer
 class Players {
   val players = new ArrayBuffer[String]
 
-  def addPlayer(name: String) = {
-    players += name
-    println("Lisäsin pelaajan!")
-    println(players.toString())
-  }
+  def addPlayer(name: String) = { players += name }
 }
--- src/main/scala/maxiYatzyCmd/CmdController.scala
+++ src/main/scala/maxiYatzyCmd/CmdController.scala
@@ -3,46 +3,28 @@ package maxiYatzyCmd
 import maxiYatzy.MaxiYatzy
 
 class CmdController {
-  private abstract class InputParser {
-    def parse(str: String): (String, InputParser)
-  }
-
-  private class NewPlayerParser(myatzy: MaxiYatzy) extends InputParser {
-    var tsparser: InputParser = null
-
-    def setTSParser(tsp: InputParser) = { tsparser = tsp }
-
-    def parse(str: String): (String, InputParser) = {
-      if (str == "ok") ("Siirrytään heittojen parsintaan!", tsparser)
-      else {
-        myatzy.addPlayer(str)
-        ("Pian osaan lopettaa pelaajanlisäyksen!", this)
-      }
-    }
-  }
-
-  private class ThrowScoreParser(myatzy: MaxiYatzy) extends InputParser {
-    def parse(str: String): (String, InputParser) = {
-      if (str == "throw") ("Pian osaan käsitellä heittoja!", this)
-      else if (str == "score") ("Pian osaan käsitellä pisteitä!", this)
-      else {
-        ("Pian osaan käsitellä tämänkin!", this)
-      }
-    }
-  }
-
   private val myatzy = new MaxiYatzy
-  private val tsparser = new ThrowScoreParser(myatzy)
-  private val npparser = new NewPlayerParser(myatzy)
-
   val initialMessage: String = "Alkuun tämä viestinpätkyläinen."
-  private var currentParser: InputParser = npparser
+  private var currentParser = parseNewPlayer _
 
-  def parse(str: String): String = {
-    val (response, nextParser) = currentParser.parse(str)
-    currentParser = nextParser
-    response
+  private def parseNewPlayer(str: String): String = {
+    if (str == "ok") {
+      currentParser = parseThrowScore
+      "Siirrytään heittojen parsintaan!"
+    }
+    else {
+      myatzy.addPlayer(str)
+      "Lisäsin pelaajan!"
+    }
   }
 
-  npparser.setTSParser(tsparser)
+  private def parseThrowScore(str: String): String = {
+    if (str == "throw") "Pian osaan käsitellä heittoja!"
+    else if (str == "score") "Pian osaan käsitellä pisteitä!"
+    else {
+      "Pian osaan käsitellä tämänkin!"
+    }
+  }
+
+  def parse(str: String): String = currentParser(str)
 }
